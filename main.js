c71349e364458a5302dfd0f80056810b86fc1d1e
Cori Fowler <fowler.cori@gmail.com>
2015-10-05 14:56:40 -0400

    Finished easy clock portion

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-// Get time variables
+setInterval( function() {
+  var date = new Date();
+  var hr = date.getHours();
+  var min = date.getMinutes();
+  var secs = date.getSeconds();
 
-var date = new Date();
-var hr = date.getHours();
-var min = date.getMinutes();
-var secs = date.getSeconds();
+  var hrs = (hr < 10) ? ('0' + hr) : hr;
+  var mins = (min < 10) ? ('0' + min) : min;
+  var seconds = (secs < 10) ? ('0' + secs) : secs;
+  var fullTimestamp = hrs + ':' + mins + ':' + seconds;
+  
+  $('#timestamp').text(fullTimestamp);
 
-var fullTimestamp = hr + ':' + min + ':' + secs;
+}, 1000);
 
-var clock = document.querySelector('#timestamp');
-var textNode = document.createTextNode(fullTimestamp);
-clock.appendChild(textNode);
+
 
